096ce8098fa4df5d20814e99c3a56c6196053479
AB Dee <devabdee@gmail.com>
2022-06-26 19:24:56 +0500

    initial commit

---
+++ ToDoList.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: aIT
+pragma solidity ^0.8.5;
+
+contract ToDoList {
+    struct Todolist {
+        string task;
+        bool done;
+    }
+
+    Todolist[] public todos;
+
+    function Create(string memory _task) public {
+        todos.push(Todolist(_task, false));
+    }
+
+    function Update(uint _index, string memory _task) public {
+        Todolist storage todo = todos[_index];
+        todo.task = _task;
+    }
+
+    function Delete(uint _index) public {
+        Todolist storage todo = todos[_index];
+        delete todo.task;
+    }
+
+    function Done(uint _index) public {
+        Todolist storage todo = todos[_index];
+        todo.done = !todo.done;
+    }
+}
