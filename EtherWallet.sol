864b3d627410a27564a36656368f512847df5e70
AB Dee <devabdee@gmail.com>
2022-06-27 11:56:48 +0500

    initial commit

---
+++ EtherWallet.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.5;
+
+contract EtherWallet {
+    address public owner = msg.sender;
+
+    function recieve() public payable {}
+
+    function send(address payable _guy, uint amount) public payable onlyOwner {
+        (bool success, ) = _guy.call{value: amount}("");
+        require(success, "Failed");
+    }
+
+    function balance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Chor! Chor! Chor!");
+        _;
+    }
+}
