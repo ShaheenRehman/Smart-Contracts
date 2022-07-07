bec9e86cedcc80b690b414ca716ccdb1f846f842
AB Dee <devabdee@gmail.com>
2022-07-07 19:26:11 +0500

    initial commit

---
+++ Sum-Array.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.5;
+
+contract SumArray {
+    function sumarray(uint[] memory array) public pure returns (uint) {
+        uint sum = 0;
+        for (uint i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+}
