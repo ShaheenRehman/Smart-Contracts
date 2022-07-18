151ac6d513dd71b49f02e09b44d4ee6e54fc8615
AB Dee <devabdee@gmail.com>
2022-07-18 20:34:32 +0500

    Open Source Contribution Contract

--- tipping.sol
+++ tipping.sol
@@ -30,134 +30,134 @@ interface ERC20 {
     ) external returns (bool success);
 }
 
-// contract tipping {
-//     using SafeMath for uint256;
-//     address public contractOwner = msg.sender;
-//     mapping(address => uint256) public balanceOf;
-//     mapping(address => bool) public admins;
+contract tipping {
+    using SafeMath for uint256;
+    address public contractOwner = msg.sender;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => bool) public admins;
 
-//     event TipMessage(
-//         address indexed recipientAddress,
-//         string message,
-//         uint256 amount
-//     );
-//     event OwnershipTransferred(
-//         address indexed previousOwner,
-//         address indexed newOwner
-//     );
+    event TipMessage(
+        address indexed recipientAddress,
+        string message,
+        uint256 amount
+    );
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
 
-//     function sendTo(address recipient_, string memory message_) public payable {
-//         (bool success, ) = recipient_.call{
-//             value: msg.value.sub(msg.value.div(100))
-//         }("");
-//         require(success, "Failed to send.");
-//         emit TipMessage(recipient_, message_, msg.value);
-//     }
+    function sendTo(address recipient_, string memory message_) public payable {
+        (bool success, ) = recipient_.call{
+            value: msg.value.sub(msg.value.div(100))
+        }("");
+        require(success, "Failed to send.");
+        emit TipMessage(recipient_, message_, msg.value);
+    }
 
-//     function sendTokenTo(
-//         address recipient_,
-//         uint256 amount_,
-//         address tokenContractAddr_,
-//         string memory message_
-//     ) public payable {
-//         ERC20 paymentTc = ERC20(tokenContractAddr_);
-//         require(
-//             paymentTc.allowance(msg.sender, address(this)) >= amount_,
-//             "Insufficient Allowance"
-//         );
+    function sendTokenTo(
+        address recipient_,
+        uint256 amount_,
+        address tokenContractAddr_,
+        string memory message_
+    ) public payable {
+        ERC20 paymentTc = ERC20(tokenContractAddr_);
+        require(
+            paymentTc.allowance(msg.sender, address(this)) >= amount_,
+            "Insufficient Allowance"
+        );
 
-//         require(
-//             paymentTc.transferFrom(msg.sender, address(this), amount_),
-//             "Transfer failed"
-//         );
+        require(
+            paymentTc.transferFrom(msg.sender, address(this), amount_),
+            "Transfer failed"
+        );
 
-//         require(
-//             paymentTc.transfer(recipient_, amount_.sub(amount_.div(100))),
-//             "Transfer failed"
-//         );
+        require(
+            paymentTc.transfer(recipient_, amount_.sub(amount_.div(100))),
+            "Transfer failed"
+        );
 
-//         emit TipMessage(recipient_, message_, amount_);
-//     }
+        emit TipMessage(recipient_, message_, amount_);
+    }
 
-//     function withdraw() external OnlyAdminCanWithdraw {
-//         (bool success, ) = msg.sender.call{value: address(this).balance}("");
-//         require(success, "Failed to withdraw.");
-//     }
+    function withdraw() external OnlyAdminCanWithdraw {
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success, "Failed to withdraw.");
+    }
 
-//     modifier OnlyAdminCanWithdraw() {
-//         if (admins[msg.sender] != true) {
-//             revert tipping__withdraw__OnlyAdminCanWithdraw();
-//         }
-//         _;
-//     }
+    modifier OnlyAdminCanWithdraw() {
+        if (admins[msg.sender] != true) {
+            revert tipping__withdraw__OnlyAdminCanWithdraw();
+        }
+        _;
+    }
 
-//     function withdrawToken(address tokenContract)
-//         external
-//         OnlyAdminCanWithdrawToken
-//     {
-//         ERC20 withdrawTC = ERC20(tokenContract);
-//         withdrawTC.transfer(msg.sender, withdrawTC.balanceOf(address(this)));
-//     }
+    function withdrawToken(address tokenContract)
+        external
+        OnlyAdminCanWithdrawToken
+    {
+        ERC20 withdrawTC = ERC20(tokenContract);
+        withdrawTC.transfer(msg.sender, withdrawTC.balanceOf(address(this)));
+    }
 
-//     modifier OnlyAdminCanWithdrawToken() {
-//         if (admins[msg.sender] != true) {
-//             revert tipping__withdrawToken__OnlyAdminCanWithdrawToken();
-//         }
-//         _;
-//     }
+    modifier OnlyAdminCanWithdrawToken() {
+        if (admins[msg.sender] != true) {
+            revert tipping__withdrawToken__OnlyAdminCanWithdrawToken();
+        }
+        _;
+    }
 
-//     function addAdmin(address adminAddress)
-//         external
-//         OnlyContractOwnerCanAddAdmins
-//     {
-//         admins[adminAddress] = true;
-//     }
+    function addAdmin(address adminAddress)
+        external
+        OnlyContractOwnerCanAddAdmins
+    {
+        admins[adminAddress] = true;
+    }
 
-//     modifier OnlyContractOwnerCanAddAdmins() {
-//         if (msg.sender != contractOwner) {
-//             revert tipping__addAdmin__OnlyContractOwnerCanAddAdmins();
-//         }
-//         _;
-//     }
+    modifier OnlyContractOwnerCanAddAdmins() {
+        if (msg.sender != contractOwner) {
+            revert tipping__addAdmin__OnlyContractOwnerCanAddAdmins();
+        }
+        _;
+    }
 
-//     function deleteAdmin(address adminAddress)
-//         external
-//         OnlyContractOwnerCanDeleteAdmins
-//     {
-//         admins[adminAddress] = false;
-//     }
+    function deleteAdmin(address adminAddress)
+        external
+        OnlyContractOwnerCanDeleteAdmins
+    {
+        admins[adminAddress] = false;
+    }
 
-//     modifier OnlyContractOwnerCanDeleteAdmins() {
-//         if (msg.sender != contractOwner) {
-//             revert tipping__deleteAdmin__OnlyContractOwnerCanDeleteAdmins();
-//         }
-//         _;
-//     }
+    modifier OnlyContractOwnerCanDeleteAdmins() {
+        if (msg.sender != contractOwner) {
+            revert tipping__deleteAdmin__OnlyContractOwnerCanDeleteAdmins();
+        }
+        _;
+    }
 
-//     // Transfer contract ownership
-//     function transferContractOwnership(address newOwner)
-//         public
-//         payable
-//         OnlyContractOwnerCanChangeOwnership
-//     {
-//         require(
-//             newOwner != address(0),
-//             "Ownable: new contractOwner is the zero address."
-//         );
-//         _transferOwnership(newOwner);
-//     }
+    // Transfer contract ownership
+    function transferContractOwnership(address newOwner)
+        public
+        payable
+        OnlyContractOwnerCanChangeOwnership
+    {
+        require(
+            newOwner != address(0),
+            "Ownable: new contractOwner is the zero address."
+        );
+        _transferOwnership(newOwner);
+    }
 
-//     modifier OnlyContractOwnerCanChangeOwnership() {
-//         if (msg.sender != contractOwner) {
-//             revert tipping__transferContractOwnership__OnlyContractOwnerCanChangeOwnership();
-//         }
-//         _;
-//     }
+    modifier OnlyContractOwnerCanChangeOwnership() {
+        if (msg.sender != contractOwner) {
+            revert tipping__transferContractOwnership__OnlyContractOwnerCanChangeOwnership();
+        }
+        _;
+    }
 
-//     // Helper function
-//     function _transferOwnership(address newOwner) internal virtual {
-//         address oldOwner = contractOwner;
-//         contractOwner = newOwner;
-//         emit OwnershipTransferred(oldOwner, newOwner);
-//     }
-// }
+    //     // Helper function
+    //     function _transferOwnership(address newOwner) internal virtual {
+    //         address oldOwner = contractOwner;
+    //         contractOwner = newOwner;
+    //         emit OwnershipTransferred(oldOwner, newOwner);
+    //     }
+}
